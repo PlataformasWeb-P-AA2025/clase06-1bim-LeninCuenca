455f5db4097c3a5b1deae4b3e92436c44d6d793c
Lenin <lacuenca16@utpl.edu.ec>
2025-05-14 08:39:53 -0500

    Avance

--- ejemplo1/agregardatoa2.py
+++ ejemplo1/agregardatoa2.py
@@ -1,6 +1,6 @@
 import csv
 from sqlalchemy.orm import sessionmaker
-from crear_base import Saludo2 as Saludo_dos
+from crear_base import Saludo
 from configuracion import engine
 
 # Crear la sesión
@@ -13,8 +13,16 @@ csv_file_path = "data/saludos_mundo.csv"
 # Leer el archivo CSV y agregar los datos a la base de datos
 with open(csv_file_path, mode='r', encoding='utf-8') as file:
     reader = csv.DictReader(file, delimiter='|')
-    saludos = [Saludo_dos(mensaje=row['saludo'], tipo=row['tipo'], origen=row['origen']) for row in reader]
-    session.add_all(saludos)
+    for row in reader:
+        # Crear un objeto Saludo con los datos del CSV
+        miSaludo = Saludo(
+            saludo=row['saludo'],
+            tipo=row['tipo'],
+            origen=row['origen']
+        )
+        # Agregar el objeto a la sesión
+        session.add(miSaludo)
 
 # Confirmar las transacciones
 session.commit()
+print("Datos agregados a la base de datos desde el archivo CSV.")
